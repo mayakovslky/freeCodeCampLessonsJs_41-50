57379023a74f8cb550de4fa2bcf660029a8f7e01
Vladimir Drozdov <mayakovsky03@gmail.com>
2022-07-22 16:31:04 +0300

    lessonsJs 41-50

---
+++ script.js
@@ -0,0 +1,68 @@
+// 41. Access Array Data with Indexes
+
+// const myArray = [50, 60, 70];
+
+// const myData = myArray[0];
+
+// 42. Modify Array Data With Indexes
+
+// const myArray = [18, 64, 99];
+
+// myArray[0] = 45;
+
+// 43. Access Multi-Dimensional Arrays With Indexes
+
+// const myArray = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+//   [[10, 11, 12], 13, 14],
+// ];
+
+// const myData = myArray[2][1];
+
+// 44. Manipulate Arrays With push()
+
+// const myArray = [["John", 23], ["cat", 2]];
+
+// myArray.push(["dog",3]);
+
+// 45. Manipulate Arrays With pop()
+
+// const myArray = [["John", 23], ["cat", 2]];
+
+// const removedFromMyArray = myArray.pop();
+
+// 46. Manipulate Arrays With shift()
+
+// const myArray = [["John", 23], ["dog", 3]];
+
+// const removedFromMyArray = myArray.shift();
+
+// 47. Manipulate Arrays With unshift()
+
+// const myArray = [["John", 23], ["dog", 3]];
+// myArray.shift();
+// myArray.unshift(["Paul", 35]);
+
+// 48. Shopping List
+
+// const myList = [['choco', 10], ['fanta', 20], ['cola', 50], ['water', 100], ['toy', 200]];
+
+// 49. Write Reusable JavaScript with Functions
+
+// function reusableFunction() {
+//   console.log("Hi World");
+// }
+
+// reusableFunction();
+
+// 50. Passing Values to Functions with Arguments
+
+function functionWithArgs(arg1, arg2) {
+  console.log(arg1 + arg2);
+} 
+
+functionWithArgs(1,2);
+
+functionWithArgs(7,9);
